db4b37d4dc42a861045564e7aff927bdb6355337
Dmitriy Rusakov <rusd80@gmail.com>
2021-11-21 12:52:12 +0300

    update app

--- flask/app.py
+++ flask/app.py
@@ -1,24 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
 application = Flask(__name__)
 
 
 @application.route("/")
 def index():
+    print(os.getcwd())
     return "Hello World! This is Flask Main Page"
 
 
 @application.route("/help")
-def helppage():
-    return "<b><font color=blue>This is Help Page v2!!!</font></b>"
+def help_page():
+    return "<b><font color=green>This is Help Page!</font></b>"
 
 
 @application.route("/htmlpage")
 def show_html_page():
-    myfile = open("mypage.html", mode='r')
-    page   = myfile.read()
-    myfile.close()
-    return page
+    my_file = open("/home/cube366/flask_ansible/templates/index.html", mode='r')
+    page = my_file.read()
+    my_file.close()
+    user = {'nickname': 'Miguel'}
+    return render_template("index.html",
+                           title='Home',
+                           user=user)
+
 
 if __name__ == "__main__":
     application.run()
